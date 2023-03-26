59334efe9799e7b21525b7d0db362cc06ee528ea
Thejas Sai Babu <84506820+sbtb225@users.noreply.github.com>
2023-03-26 16:05:06 +0200

    Create boiler.js

---
+++ boiler.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Boiler from './Boiler';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Boiler />
+    
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+reportWebVitals();
